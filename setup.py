92fc8c362529d2fad0064d9ebf51168796dacc26
thiagonebuloni <100470956+thiagonebuloni@users.noreply.github.com>
2022-10-26 08:14:43 -0300

    updating with package files

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="jokenpo game",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/thiagonebuloni/jokenpo"
+    url="https://github.com/thiagonebuloni/jokenpo",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
